324f613e6371c291f10c3daa4b1e9a235cb7c22d
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-04-05 00:07:49 -0400

    Again.

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,14 +2,17 @@ import { Route, Routes } from 'react-router'
 import RegisterPage from '../auth/register/RegisterPage'
 import HomePage from '../views/home/HomePage'
 import LoginPage from '../auth/login/LoginPage'
+import LayoutComponent from '../components/layout/LayoutComponent'
 
 const AppRouter = () => {
   return (
-   <Routes>
-      <Route path='/' element={<HomePage />}/>
-      <Route path='/login' element={<LoginPage />}/>
-      <Route path='/register' element={<RegisterPage />}/>
-   </Routes>
+    <LayoutComponent>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/register' element={<RegisterPage />}/>
+    </Routes>
+    </LayoutComponent>
   )
 }
 
