3465955402d357c2551c631c9b350a52d227108f
DarkHamlet20 <149280380+DarkHamlet20@users.noreply.github.com>
2024-04-05 20:57:58 -0400

    quote change

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -74,7 +74,7 @@ export default function LoginComponent() {
                       type="submit" 
                       className="w-full text-white bg-red-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                       <p className="text-sm font-light text-black">
-                          Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                          Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                       </p>
                       
                   </form>
