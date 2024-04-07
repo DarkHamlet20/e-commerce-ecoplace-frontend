1862785552f4e4f709b37a88f8023d59d5d9347c
Enyel Liranzo <xardimodschannel@gmail.com>
2024-04-06 23:11:53 -0400

    feat: adding useParams to fetch data and show to view

--- src/views/home/ProductPage.jsx
+++ src/views/home/ProductPage.jsx
@@ -1,37 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import LayoutComponent from '../../layout/LayoutMain'
 import ProductComponent from '../../components/ProductComponent'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
+
 const ProductPage = () => {
 
-  const [data, setData] = useState({})
-
-  const getData = async () => {
-    try {
-      const response = await axios.get('https://ecoplace.3.us-1.fl0.io/products')
-      setData(response.data)
-    }
-    catch(err) {
-      console.log('Advertesiment '+ err);
-    }
-  }
-  console.log(data);
+  const { id } = useParams();
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    getData()
-  }, [])
+    const getData = async () => {
+      try {
+        const response = await axios.get(`https://ecoplace.3.us-1.fl0.io/products/${id}`);
+        setData(response.data)
+      }
+      catch (err) {
+        console.log('Advertesiment ' + err);
+      }
+    };
 
+    getData();
+  }, [id])
 
   return (
     <>
       <LayoutComponent>
-      <ProductComponent
-            name={data.name}
-            id={data._id}
-            img={data.images[0]}
-            brand={data.brand}
-            price={data.price}
-            description={data.description} />
+        <ProductComponent
+          name={data?.name}
+          id={data?._id}
+          img={data?.images[0]}
+          brand={data?.brand}
+          price={data?.price}
+          description={data?.description} />
       </LayoutComponent>
     </>
   )
