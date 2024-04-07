a9c8a661eec5706e88e77223bb17780d5a7ad542
Enyel Liranzo <xardimodschannel@gmail.com>
2024-04-06 23:49:40 -0400

    update: changing redirections

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -60,7 +60,7 @@ const HeaderComponent = () => {
               </div>
               <ul className="py-2" aria-labelledby="user-menu-button">
                 <li>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                  <a href="/user" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
                 </li>
                 <li>
                   <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
