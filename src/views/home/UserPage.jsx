1c6dcbc4189a86b77d0c2592e56cdcb44dc14157
Enyel Liranzo <xardimodschannel@gmail.com>
2024-04-06 23:50:16 -0400

    feat: adding UserPage data to make fetch to user

--- src/views/home/UserPage.jsx
+++ src/views/home/UserPage.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+// import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 const UserPage = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const token = localStorage.getItem('auth_token');
+
+  useEffect(() => {
+    // Obtener el valor de la cookie de autenticación desde el localStorage
+
+    // Verificar si la cookie de autenticación está presente y es válida
+    if (!token) {
+      // Puedes hacer cualquier verificación adicional aquí, como validar el token con el servidor
+      setAuthenticated(false);
+    }
+
+    setAuthenticated(true);
+
+    axios.get('https://api.example.com/data', {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+        // Otros encabezados opcionales según sea necesario
+      }
+    }).then(
+      response => setUserData(response.data)
+    )
+      .catch(error => {
+        // Manejar errores de la solicitud
+        console.error('There was an error with the request:', error);
+      });
+  }, [token]);
+
+  // Redirigir al usuario a la página de inicio de sesión si no está autenticado
+  // if (!authenticated) {
+  //   return <Redirect to="/login" />;
+  // }
+
+  // Si el usuario está autenticado, renderizar el componente privado
   return (
     <>
-      
+      <div>
+        <p>Nombre: <span>{userData?.name}</span></p>
+        <p>Apellido: <span>{userData?.lastname}</span></p>
+      </div>
     </>
   )
 }
