12a34dbcc58d379f2c0ea71e3ce7cff6ce8ca81e
romariofamilia <128868080+romariofamilia@users.noreply.github.com>
2024-04-10 21:56:14 -0400

    [update] colors

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -79,7 +79,7 @@ const NavComponent = ({ handleSide }) => {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img src={logo} className="mr-3 rounded-full h-16" alt="EcoPlace Logo" />
-            <span className="text-xl font-semibold whitespace-nowrap dark:text-white">EcoPlace</span>
+            <span className="text-xl font-semibold whitespace-nowrap text-white">EcoPlace</span>
           </NavLink>
           <div className="flex mt-4 md:mt-0">
             <SearchComponent />
@@ -87,7 +87,7 @@ const NavComponent = ({ handleSide }) => {
 
           <div className="flex relative z-50 text-white items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse mt-4 md:mt-0">
             <div className="mr-4">
-              <button onClick={redirectToCart} className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300">
+              <button onClick={redirectToCart} className="text-white  hover:text-gray-300">
                 <FontAwesomeIcon icon={faShoppingCart} size="lg" />
               </button>
             </div>
@@ -95,7 +95,7 @@ const NavComponent = ({ handleSide }) => {
               <button
                 onClick={() => setShow(!show)}
                 type="button"
-                className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-600"
                 id="user-menu-button"
                 aria-expanded="false"
                 data-dropdown-toggle="user-dropdown"
@@ -121,15 +121,15 @@ const NavComponent = ({ handleSide }) => {
             </div>
 
             <div
-              className={`"z-50 top-16 right-1 absolute my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 " id="user-dropdown" ${show ? "block" : "hidden"
+              className={`"z-50 top-16 right-1 absolute my-4 text-base list-none divide-y  rounded-lg shadow bg-gray-700 divide-gray-600 " id="user-dropdown" ${show ? "block" : "hidden"
                 }`}
             >
 
               <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">
+                <span className="block text-sm text-white">
                   {userData.name} {userData.lastname}
                 </span>
-                <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                <span className="block text-sm truncate text-gray-400">
                   {userData.email}
                 </span>
               </div>
@@ -137,7 +137,7 @@ const NavComponent = ({ handleSide }) => {
                 <li>
                   <a
                     href="/user"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
                   >
                     Acount
                   </a>
@@ -148,7 +148,7 @@ const NavComponent = ({ handleSide }) => {
                     : <li>
                       <a
                         href={`/seller`}
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
                       >
                         Dashboard
                       </a>
@@ -157,7 +157,7 @@ const NavComponent = ({ handleSide }) => {
                 <li>
                   <NavLink
                     to="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    className="block px-4 py-2 text-sm   hover:bg-gray-600 text-gray-200 hover:text-white"
                   >
                     Settings
                   </NavLink>
@@ -165,7 +165,7 @@ const NavComponent = ({ handleSide }) => {
                 <li>
                   <NavLink
                     to="/orders"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
                   >
                     Orders
                   </NavLink>
@@ -174,7 +174,7 @@ const NavComponent = ({ handleSide }) => {
                   <NavLink
                     to="/login"
                     onClick={() => handleLogout()}
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    className="block px-4 py-2 text-sm   hover:bg-gray-600 text-gray-200 hover:text-white"
                   >
                     Sign out
                   </NavLink>
@@ -185,7 +185,7 @@ const NavComponent = ({ handleSide }) => {
               onClick={() => setDrop(!drop)}
               data-collapse-toggle="navbar-user"
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg lg:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
               aria-controls="navbar-user"
               aria-expanded="false"
             >
@@ -215,7 +215,7 @@ const NavComponent = ({ handleSide }) => {
               <li>
                 <NavLink
                   to="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 md:text-blue-500"
                   aria-current="page"
                 >
                   Home
@@ -224,7 +224,7 @@ const NavComponent = ({ handleSide }) => {
               <li>
                 <NavLink
                   to="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 rounded md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
                 >
                   About
                 </NavLink>
@@ -233,7 +233,7 @@ const NavComponent = ({ handleSide }) => {
               <li>
                 <NavLink
                   to="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 rounded md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
                 >
                   Contact
                 </NavLink>
