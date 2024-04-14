06ef45c1b2e45fbc48cb135c0c7743a2279c8a26
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-04-14 14:10:45 -0400

    Fixing Nav Component.

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,26 +1,16 @@
-/* eslint-disable react/prop-types */
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, NavLink } from "react-router-dom";
 import axios from "axios";
-import { NavLink } from "react-router-dom";
-import { SearchComponent } from "./SearchComponent";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import logo from '../../public/img/DALL·E_2024_03_31_20_04_37_Create_an_illustrative_logo_for_EcoPlace (1).webp';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../public/img/DALL·E_2024_03_31_20_04_37_Create_an_illustrative_logo_for_EcoPlace (1).webp";
 import { roles } from "../types/roles";
 
 const NavComponent = ({ handleSide }) => {
   const navigate = useNavigate();
-
-  // const handleUser = () => {
-  //   navigate('/user')
-  // }
-
-  const redirectToCart = () => {
-    navigate("/cart");
-  };
-
   const [userData, setUserData] = useState({});
+  const [show, setShow] = useState(false);
+  const [drop, setDrop] = useState(false);
   const token = localStorage.getItem("auth_token");
 
   useEffect(() => {
@@ -35,18 +25,12 @@ const NavComponent = ({ handleSide }) => {
         .then((response) => setUserData(response.data))
         .catch((error) => {
           console.error("There was an error with the request:", error);
-          // Optionally, you can handle errors here or show a message to the user
         });
     }
   }, [token]);
 
   const handleLogout = async () => {
     try {
-      const token = localStorage.getItem("auth_token");
-
-      if (!token) {
-        console.error("No se encontro el token de autenticacion.");
-      }
       await axios.post(
         "http://localhost:3000/users/logout",
         {},
@@ -58,16 +42,11 @@ const NavComponent = ({ handleSide }) => {
       );
       localStorage.removeItem("auth_token");
       localStorage.removeItem("userRole");
-      // Si la petición es exitosa, elimina el token de localStorage y llama a onLogout
       navigate("/login");
     } catch (error) {
       console.error("Error al cerrar sesión:", error);
-      // Manejar cualquier error aquí, por ejemplo, mostrando un mensaje al usuario
     }
   };
-
-  const [show, setShow] = useState(false);
-  const [drop, setDrop] = useState(false);
 
   return (
     <>
@@ -76,19 +55,29 @@ const NavComponent = ({ handleSide }) => {
           {/* Logo */}
 
           <div>
-          <NavLink
+            <NavLink
               to="/"
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
-              <img src={logo} className="mr-3 rounded-full h-16" alt="EcoPlace Logo" />
-              <span className="text-xl font-semibold whitespace-nowrap text-white">EcoPlace</span>
+              <img
+                src={logo}
+                className="mr-3 rounded-full h-16"
+                alt="EcoPlace Logo"
+              />
+              <span className="text-xl font-semibold whitespace-nowrap text-white">
+                EcoPlace
+              </span>
             </NavLink>
           </div>
 
           {/* NavButton */}
 
           <div>
-            <ul className={`smm:flex md:w-96 right-16 top-24 flex-row smm:bg-transparent rounded bg-gray-800 items-center smm:static absolute md:mt-0 justify-around ${drop ? 'flex' : 'hidden'} `}>
+            <ul
+              className={`smm:flex md:w-96 right-16 top-24 flex-row smm:bg-transparent rounded bg-gray-800 items-center smm:static absolute md:mt-0 justify-around ${
+                drop ? "flex" : "hidden"
+              } `}
+            >
               <li>
                 <NavLink
                   to="/"
@@ -125,7 +114,10 @@ const NavComponent = ({ handleSide }) => {
 
           <div className="flex relative z-50 text-white items-center space-x-3 md:space-x-0 rtl:space-x-reverse">
             <div className="mr-4">
-              <button onClick={redirectToCart} className="text-white  hover:text-gray-300">
+              <button
+                onClick={() => navigate("/cart")}
+                className="text-white  hover:text-gray-300"
+              >
                 <FontAwesomeIcon icon={faShoppingCart} size="lg" />
               </button>
             </div>
@@ -154,17 +146,14 @@ const NavComponent = ({ handleSide }) => {
                     d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                   />
                 </svg>
-
               </button>
             </div>
 
-            
-
             <div
-              className={`"z-50 top-16 right-1 absolute my-4 text-base list-none divide-y  rounded-lg shadow bg-gray-700 divide-gray-600 " id="user-dropdown" ${show ? "block" : "hidden"
-                }`}
+              className={`"z-50 top-16 right-1 absolute my-4 text-base list-none divide-y  rounded-lg shadow bg-gray-700 divide-gray-600 " id="user-dropdown" ${
+                show ? "block" : "hidden"
+              }`}
             >
-
               <div className="px-4 py-3">
                 <span className="block text-sm text-white">
                   {userData.name} {userData.lastname}
@@ -182,18 +171,18 @@ const NavComponent = ({ handleSide }) => {
                     Acount
                   </a>
                 </li>
-                {
-                  (userData?.role !== roles.seller)
-                    ? ''
-                    : <li>
-                      <a
-                        href={`/seller`}
-                        className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
-                      >
-                        Dashboard
-                      </a>
-                    </li>
-                }
+                {userData?.role !== roles.seller ? (
+                  ""
+                ) : (
+                  <li>
+                    <a
+                      href={`/seller`}
+                      className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
+                    >
+                      Dashboard
+                    </a>
+                  </li>
+                )}
                 <li>
                   <NavLink
                     to="#"
@@ -211,13 +200,21 @@ const NavComponent = ({ handleSide }) => {
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink
-                    to="/login"
-                    onClick={() => handleLogout()}
-                    className="block px-4 py-2 text-sm   hover:bg-gray-600 text-gray-200 hover:text-white"
-                  >
-                    Sign out
-                  </NavLink>
+                  {!token ? (
+                    <NavLink
+                      to="/login"
+                      className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
+                    >
+                      Sign In
+                    </NavLink>
+                  ) : (
+                    <NavLink
+                      onClick={handleLogout}
+                      className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
+                    >
+                      Sign Out
+                    </NavLink>
+                  )}
                 </li>
               </ul>
             </div>
@@ -248,7 +245,6 @@ const NavComponent = ({ handleSide }) => {
             </button>
           </div>
         </div>
-
       </nav>
       <div>
         <h1
