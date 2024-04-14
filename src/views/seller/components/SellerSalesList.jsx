de9dd97bb989e5b4e6443b93384abd8abeb88f36
Enyel Liranzo <xardimodschannel@gmail.com>
2024-04-14 15:40:51 -0400

    update: adding await object properties

--- src/views/seller/components/SellerSalesList.jsx
+++ src/views/seller/components/SellerSalesList.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+// import React from "react";
 
+// eslint-disable-next-line react/prop-types
 const SellerSalesList = ({ sales }) => {
   return (
     <div className="overflow-hidden shadow-md mt-6">
@@ -9,18 +11,18 @@ const SellerSalesList = ({ sales }) => {
             <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Cliente</th>
             <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Fecha de Venta</th>
             <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Total</th>
-            <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Acciones</th>
+            {/* <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Acciones</th> */}
           </tr>
         </thead>
         <tbody className="bg-white">
-          {sales.map((sale) => (
-            <tr key={sale._id} className="hover:bg-gray-100">
-              <td className="py-3 px-4">{`${sale.customer.name} ${sale.customer.lastname}`}</td>
-              <td className="py-3 px-4">{new Date(sale.dateOfSale).toLocaleDateString()}</td>
-              <td className="py-3 px-4">{`$${sale.total.toFixed(2)}`}</td>
-              <td className="py-3 px-4">
+          {sales && sales.map((sale) => (
+            <tr key={sale?._id} className="hover:bg-gray-100">
+              <td className="py-3 px-4">{`${sale?.customer?.name} ${sale?.customer?.lastname}`}</td>
+              <td className="py-3 px-4">{new Date(sale?.dateOfSale).toLocaleDateString()}</td>
+              <td className="py-3 px-4">{`$${sale?.total?.toFixed(2)}`}</td>
+              {/* <td className="py-3 px-4">
                 <button className="text-blue-500 hover:text-blue-600">Ver detalles</button>
-              </td>
+              </td> */}
             </tr>
           ))}
         </tbody>
